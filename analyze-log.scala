541baa7611f8d1d7d5031ff35a872e73be6db40a
Kevin Duraj <kduraj@gmail.com>
2016-10-08 17:33:52 -0700

    2016-10-08 17:33

--- analyze-log.scala
+++ analyze-log.scala
@@ -30,21 +30,21 @@ logDF.registerTempTable("log_table")
 val query1 = """SELECT ip, COUNT(*) AS total FROM log_table GROUP BY ip ORDER BY total DESC LIMIT 7"""
 sql(query1).show()
 
-//val query2 = """SELECT ip, SUM(size) AS bigsize FROM log_table GROUP BY ip ORDER BY bigsize DESC LIMIT 7"""
-//sql(query2).show(7, false)
+val query2 = """SELECT ip, SUM(size) AS bigsize FROM log_table GROUP BY ip ORDER BY bigsize DESC LIMIT 7"""
+sql(query2).show(7, false)
 
-//val query2 = """SELECT ref, COUNT(*) AS total FROM log_table GROUP BY ref ORDER BY total DESC LIMIT 12"""
-//sql(query2).show(12, false)
+val query2 = """SELECT ref, COUNT(*) AS total FROM log_table GROUP BY ref ORDER BY total DESC LIMIT 12"""
+sql(query2).show(12, false)
 
 /*-------------------------------------------------------------------------------------------------------------------
     Analyze Bot visits
  -------------------------------------------------------------------------------------------------------------------*/
 
-//val botDF = lines.map(_.split("\\|")).filter(_(3).contains("bot")).map( r => HttpLog(r(0), validateSize(r(1)), r(2), r(3), r(4), r(5), r(6) ) ).toDF()
-//botDF.registerTempTable("bot_table")
+val botDF = lines.map(_.split("\\|")).filter(_(3).contains("bot")).map( r => HttpLog(r(0), validateSize(r(1)), r(2), r(3), r(4), r(5) ) ).toDF()
+botDF.registerTempTable("bot_table")
 
-//val query3 = """SELECT host, COUNT(*) AS total FROM bot_table GROUP BY host ORDER BY total DESC LIMIT 12"""
-//sql(query3).show(12,false)
+val query3 = """SELECT host, COUNT(*) AS total FROM bot_table GROUP BY host ORDER BY total DESC LIMIT 12"""
+sql(query3).show(12,false)
 
 println("echo > " + file)
 
